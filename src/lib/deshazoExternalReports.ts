6aa7e62e127ff3e4f3d447bd7bdfe1277c304f97
RashmiAher <aher604@gmail.com>
2026-05-29 01:30:15 -0700

    Update deshazoExternalReports.ts

--- src/lib/deshazoExternalReports.ts
+++ src/lib/deshazoExternalReports.ts
@@ -344,7 +344,7 @@ export async function syncDeshazoExternalWorkOrders(options: { pageSize: number;
     method: 'POST',
     headers: {
       Accept: 'application/json',
-      'X-API-Key': deshazoExternalApiKey,
+      Authorization: `Bearer ${deshazoExternalApiKey}`,
     },
   })
 
